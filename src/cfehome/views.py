cf5f8ba09c736deb3418c56aa56581835830c7aa
Deepkshith <deepkshithnandagopal@gmail.com>
2025-07-29 23:16:09 +0530

    Added all files

---
+++ src/cfehome/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def hello_world(request):
+    return render(request, "hello-world.html", {})
+
+
+
+
+def healthz_view(request):
+    return HttpResponse("ok")
